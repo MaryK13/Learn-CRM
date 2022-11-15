56ea6e648ad3a1eb7c11cee2305aa7eb45ea5ee6
Maria Kozycheva <jane.gacy@icloud.com>
2022-11-15 12:28:21 +0300

    Update button-save

--- button-save.js
+++ button-save.js
@@ -1,34 +1,64 @@
-var fieldName = Xrm.Page.getAttribute('fullname').getValue();
-var fieldEmail = Xrm.Page.getAttribute('emailaddress1').getValue();
-var fieldRegion = Xrm.Page.getAttribute('new_contactregion').getValue();
-var fieldCity = Xrm.Page.getAttribute('new_contactcity').getValue();
 var SaveButton = {
 
 	SaveElements: function () {
+		var fieldName = Xrm.Page.getAttribute('fullname').getValue();
+		var fieldEmail = Xrm.Page.getAttribute('emailaddress1').getValue();
+		var fieldRegion = Xrm.Page.getAttribute('new_contactregion').getValue();
+		var fieldCity = Xrm.Page.getAttribute('new_contactcity').getValue();
+		var elements = [
+			{ 'field': fieldName, 'fieldTxt': 'Полное имя' },
+			{ 'field': fieldEmail, 'fieldTxt': 'Электронная почта' },
+			{ 'field': fieldRegion, 'fieldTxt': 'Регион' },
+			{ 'field': fieldCity, 'fieldTxt': 'Город' }
+		];
+
 		if (fieldName && fieldEmail && fieldRegion && fieldCity) {
-			// var btn = Xrm.Page.getEventArgs().getSaveMode();
-			// console.log(btn)
-
-			Xrm.Page.data.refresh(true).then(() => {
-				return Xrm.Page.data.save();
-			}, error => {
-				console.log(error)
-			})
-
-			// Xrm.Page.data.save();
-			console.log(fieldName, fieldEmail, fieldRegion, fieldCity);
+			Xrm.Page.data.save().then(() => {
+				var entity = Xrm.Page.data.entity.getEntityName();
+				var entityId = Xrm.Page.data.entity.getId();
+				var entityFormOptions = {
+					'entityName': entity,
+					'entityId': entityId
+				};
+				Xrm.Navigation.openForm(entityFormOptions).then(success => console.log(success), error => console.log(error));
+			}, error => console.log(error));
 		} else {
-			alert('Заполните обязательные поля!')
+			var alertTxt = `Заполните обязательные поля!\n`;
+			var fieldTxts = [];
+			elements.map(x => {
+				if (x.field === null) {
+					fieldTxts.push(x.fieldTxt);
+				}
+			});
+			alert(alertTxt.concat(fieldTxts));
 		}
 	},
 
 	SaveElementsAndClose: function () {
-		if (fieldName && fieldEmail && fieldRegion && fieldCity) {
-			Xrm.Page.data.save();
+		var fieldName = Xrm.Page.getAttribute('fullname').getValue();
+		var fieldEmail = Xrm.Page.getAttribute('emailaddress1').getValue();
+		var fieldRegion = Xrm.Page.getAttribute('new_contactregion').getValue();
+		var fieldCity = Xrm.Page.getAttribute('new_contactcity').getValue();
+		var elements = [
+			{ 'field': fieldName, 'fieldTxt': 'Полное имя' },
+			{ 'field': fieldEmail, 'fieldTxt': 'Электронная почта' },
+			{ 'field': fieldRegion, 'fieldTxt': 'Регион' },
+			{ 'field': fieldCity, 'fieldTxt': 'Город' }
+		];
 
-			console.log(fieldName, fieldEmail, fieldRegion, fieldCity);
+		if (fieldName && fieldEmail && fieldRegion && fieldCity) {
+			Xrm.Page.data.save().then(() => {
+				Xrm.Page.ui.close();
+			}, error => console.log(error));
 		} else {
-			alert('Заполните обязательные поля!')
+			var alertTxt = `Заполните обязательные поля!\n`;
+			var fieldTxts = [];
+			elements.map(x => {
+				if (x.field === null) {
+					fieldTxts.push(x.fieldTxt);
+				}
+			});
+			alert(alertTxt.concat(fieldTxts));
 		}
 	}
 }
